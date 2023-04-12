eb2fa77bde8f8d6ed7749ce5db67da6037f3e277
Куцерубова Влада <80004001+vlada98K@users.noreply.github.com>
2023-04-12 17:43:35 +0500

    Update task04.js

--- Задачи/task04.js
+++ Задачи/task04.js
@@ -1,1 +1,16 @@
+import DictUsers from 'users.json';
+import FullLodash from 'lodash.js';
 
+function FuncTask4(colors) {
+    const ListColorAndKeys = [];
+    for (let i of colors) {
+        const [CurrentColor] = Object.entries(i);
+        ListColorAndKeys.push({
+            "colour" : CurrentColor[0], "rgb" : CurrentColor[1].slice(0, 3) });
+    }
+
+    return FullLodash.sortBy(colorsWithNewKeys, 'color');
+}
+
+const Result = FuncTask4(DictUsers);
+console.log(Result);
