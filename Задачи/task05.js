b8d549b3818969ba215e06a5b5b9c7debb4426c7
Куцерубова Влада <80004001+vlada98K@users.noreply.github.com>
2023-04-12 18:31:06 +0500

    Update task05.js

--- Задачи/task05.js
+++ Задачи/task05.js
@@ -1,1 +1,22 @@
+const {colors, argb} = require('data');
+import FullLodash from 'lodash.js';
 
+function rgbToHex([r, g, b]) {
+    function colorToHex(color) {
+        return color.toString(16).length == 1 ? '0' + color.toString(16) : color.toString(16);
+    }
+
+    return `#${colorToHex(r)}${colorToHex(g)}${colorToHex(b)}`
+}
+
+function FuncTask5(colors, argb) {
+    const hexes = argb.map(rgb => rgbToHex(rgb));
+    const consolidatedArray = colors.map((color, i) => {
+        return {"color" : color, "hex_name" : hexes[i]}
+    });
+
+    return FullLodash.sortBy(consolidatedArray, 'color');
+}
+
+const Result = FuncTask5(colors, argb);
+console.log(Result);
