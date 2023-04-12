0a1074e1b5336a4a7ec50db61dca795000b5dc0c
Куцерубова Влада <80004001+vlada98K@users.noreply.github.com>
2023-04-12 15:02:30 +0500

    Update task02.js

--- Задачи/task02.js
+++ Задачи/task02.js
@@ -1,1 +1,15 @@
+import DictUsers from 'users.json';
+import FullLodash from 'lodash.js';
 
+function FuncTask2(DictOfUsers) {
+    const SelectedUsers = DictOfUsers['clients'];
+
+    const ResultOfUsers = SelectedUsers.filter(User => {
+        return User['address']['city'].toLowerCase() === 'кунгур';
+    });
+
+    return FullLodash.orderBy(ResultOfUsers, ['gender', 'age', 'name'], ['asc', 'desc', 'asc']);
+}
+
+const Result = FuncTask2(DictUsers);
+console.log(Result);
