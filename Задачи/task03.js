d809965d646fe686879f48dabe95207c3cbd068d
Куцерубова Влада <80004001+vlada98K@users.noreply.github.com>
2023-04-12 16:51:34 +0500

    Update task03.js

--- Задачи/task03.js
+++ Задачи/task03.js
@@ -1,1 +1,14 @@
+import DictUsers from 'users.json';
 
+function FuncTask3(colors) {
+    const ListOfColors = [];
+    for (let i of colors) {
+        if (Object.keys(i)[0].length < 6) {
+            ListOfColors.push(Object.keys(i)[0]);
+        }
+    }
+
+    return ListOfColors.sort();
+}
+const Result = FuncTask3(DictUsers);
+console.log(Result);
