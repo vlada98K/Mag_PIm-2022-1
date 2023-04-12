486c63bb33fac676c1f9cb39b04f000e03f65aab
Куцерубова Влада <80004001+vlada98K@users.noreply.github.com>
2023-04-12 14:36:55 +0500

    Update task01.js

--- Задачи/task01.js
+++ Задачи/task01.js
@@ -1,1 +1,18 @@
+import DictUsers from 'users.json'
+import FullLodash from 'lodash.js'
 
+function FuncTask1(DictOfUsers) {
+    const SelectedUsers = DictOfUsers.filter(
+        User => Number(User.address['geo']['lat']) < 0);
+
+    const ResultDict = [];
+    SelectedUsers.forEach(user => {
+        ResultDict.push(
+            { "username": user.username,"city": user.address.city});
+    });
+
+    return FullLodash.orderBy(ResultDict, 'city', 'desc');
+}
+
+const Result = FuncTask1(DictUsers)
+console.log(Result);
